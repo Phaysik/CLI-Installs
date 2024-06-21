37056b2d4e6f5cefbf62ac1fd1bc5107524fe091
Phaysik <mmoore0111@gmail.com>
2024-06-21 01:42:54 -0700

    Initial commit

---
+++ cliTools.sh
@@ -0,0 +1,79 @@
+setUpZShell() {
+    sudo apt install zsh -y
+    sudo chsh -s $(which zsh)
+    touch ~/.zshrc
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+    git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+    git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
+    git clone https://github.com/fdellwing/zsh-bat.git $ZSH_CUSTOM/plugins/zsh-bat
+    git clone https://github.com/GeoLMg/apt-zsh-plugin.git $ZSH_CUSTOM/plugins/apt
+    mv ./.zshrc ~/.zshrc
+
+    source "$ZSH/oh-my-zsh.sh"
+
+    echo "You'll need to download the powerlevel10k font from https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k"
+
+    echo "After that run p10k configure"
+
+    exec zsh
+}
+
+setUpDust() {
+    if [ -x "$(command -v dust)" ]; then
+        echo "dust exists"
+    else
+        curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+        # Download the latest version of Tracy
+        url=$(curl -s https://api.github.com/repos/bootandy/dust/releases/latest | grep -o '"tarball_url": *"[^"]*"' | cut -d '"' -f 4)
+        curl -L -o dust-latest.tar.gz $url
+        mkdir -p dust-latest
+        tar -xvzf dust-latest.tar.gz -C dust-latest --strip-components=1
+        sudo rm -rf dust-latest.tar.gz
+        cd dust-latest
+
+        source $HOME/.cargo/env
+        cargo install du-dust
+
+        cd ..
+
+        rm -rf dust-latest
+    fi
+}
+
+setUpBat() {
+    if [ -x "$(command -v bat)" ]; then
+        echo "bat exists"
+    else
+        sudo apt install bat -y
+
+        mkdir -p ~/.local/bin
+        ln -s /usr/bin/batcat ~/.local/bin/bat
+
+        mv ./bat-config /home/phaysik/.config/bat/config
+    fi
+}
+
+setUpFzf() {
+    if [ -x "$(command -v fzf)" ]; then
+        echo "fzf exists"
+    else
+        sudo apt install fzf -y
+    fi
+}
+
+main() {
+    setUpZShell
+
+    # setUpDust
+
+    # setUpBat
+
+    setUpFzf
+}
+
+main "$@"
