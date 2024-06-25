bdf588b0dd14daaa7881c26e2de872c7f8d1e4d4
Phaysik <mmoore0111@gmail.com>
2024-06-25 10:44:44 -0700

    Add zoxide install

--- cliTools.sh
+++ cliTools.sh
@@ -3,6 +3,8 @@ setUpZShell() {
     sudo chsh -s $(which zsh)
     touch ~/.zshrc
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
